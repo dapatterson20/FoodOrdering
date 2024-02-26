7f74127f73669a39884e2d2411cf49f94c812f27
ktchemi20 <77713295+ktchemi20@users.noreply.github.com>
2024-02-26 16:32:41 -0500

    comments for index.tsx completed

--- src/app/(admin)/index.tsx
+++ src/app/(admin)/index.tsx
@@ -1,5 +1,10 @@
+// This imports the "Redirect" component from the module "expo-router".
+// This allows it to immediately redirect from a particular screen to another.
 import {Redirect} from 'expo-router';
 
+// The "TabIndex" component is being exported as the default export, 
+// meaning when other files import this module they will get this component by default.
 export default function TabIndex() {
+    // When "TabIndex" is called, it will redirect the user to the "/admin/menu/" screen.
     return <Redirect href={'/(admin)/menu/'}/>
 }
